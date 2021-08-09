bbe2479d59f95b184f50c1421465a52b084aea27
ajaykaju <ajukodinhi@gmail.com>
2021-08-10 02:17:46 +0530

    first commit

---
+++ src/components/NavBar.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="navbar">
+    <div class="logo">
+      <img src="../assets/icons/grapes.svg" />
+      grapeslab
+    </div>
+    <div class="routes">
+      <div class="item" style="cursor: pointer;">Home</div>
+      <div class="item">Trainer</div>
+      <div class="item">Programs</div>
+      <div class="item">Blog</div>
+      <div class="item">Pricing</div>
+    </div>
+    <div class="configure">
+      <img src="../assets/icons/search.svg" />
+      <img src="../assets/icons/cart.svg" />
+      <div class="button">Log In</div>
+    </div>
+  </div>
+</template>
+
+<style lang="sass" scoped>
+@import "../styles/navbar"
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,45 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <section class="sec1">
+      <nav-bar class="nav"></nav-bar>
+      <div class="background">
+        <div class="b1"></div>
+        <div class="b2"></div>
+      </div>
+      <div class="content">
+        <div class="content1">
+          <div class="title">Gym Gives You The Perfect Health</div>
+          <div class="description">
+            It is a long established fact that a reader will be by readable
+            content of a page when are best product
+          </div>
+          <div class="actions">
+            <div class="shop_button">Shop now</div>
+            <div class="watch_videos"><div class="icon"><img src="../assets/icons/play.svg"></div> Watch videos</div>
+          </div>
+        </div>
+        <div class="content2">
+          <img src="../assets/dumbbell.png">
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: 'Home',
-  components: {
-    HelloWorld,
-  },
+  components: { NavBar },
+  //   setup() {
+
+  //   },
 });
 </script>
+
+
+
+<style lang="sass" scoped>
+@import "../styles/home"
+</style>
